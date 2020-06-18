99fe45eb5885a9e50c23d27d2e6480573d1a6488
GabrielHenrique <gabrielhenriqueg@users.noreply.github.com>
2020-06-18 16:12:25 -0300

    uptade

--- js/index.js
+++ js/index.js
@@ -1,12 +1,13 @@
 var toggle = document.getElementById("toggle-click");
 var display = document.getElementById("navbar-display");
+var item = document.getElementsByClassName("nav-item");
 
 toggle.addEventListener("click", function () {
-  if ((display.style.display === "none")) {
-    display.style.display = "flex";
-    display.style.position = "absolute"
-    display.style.marginTop = "220px"
+  if (display.style.height === "0px") {
+    display.style.height = "50vh";
+    display.style.visibility = "visible";
   } else {
-    display.style.display = "none";
+    display.style.height = "0px";
+    display.style.visibility = "hidden";
   }
 });
