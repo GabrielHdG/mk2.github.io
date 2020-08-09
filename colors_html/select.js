3e82040019275c18bdba6b4ac980a8348aa97772
GabrielHenrique <gabrigodoy10@gmail.com>
2020-08-09 18:45:04 -0300

    Update

---
+++ colors_html/select.js
@@ -0,0 +1,7 @@
+var containerImg = document.getElementsByClassName("img-container");
+
+for (let i = 0; i < containerImg.length; i++) {
+  containerImg[i]
+    .querySelector("img")
+    .addEventListener("click", function teste() {});
+}
